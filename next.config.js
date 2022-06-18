276a9b71cb49c030793611919af278043f67b0bb
ekdldksp123 <ekdldksp123@naver.com>
2022-06-18 10:26:43 +0900

    :wrench: add eslintrc, prettierc, and stylelintrc but an error occurs :(

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+const productionUrl = 'https://ekdldksp123.github.io/mayb-front'
+
 const nextConfig = {
   reactStrictMode: true,
+  staticPageGenerationTimeout: 120,
+  images: {
+    loader: 'akamai',
+    path: '',
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [1200, 1920, 2048, 3840],
+  },
+  assetPrefix: isProd ? productionUrl : '',
+  pageExtentions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
